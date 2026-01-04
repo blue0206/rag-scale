0377c004047008c304242aa766e280a80602fa98
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-04 16:13:45 +0530

    feat: add graph db and model related env

--- backend-ai/src/core/config.py
+++ backend-ai/src/core/config.py
@@ -3,9 +3,15 @@ import os
 
 load_dotenv()
 
-if not os.getenv("GROQ_API_KEY"):
-    raise ValueError("Missing environment variables.")
+if not os.getenv("GROQ_API_KEY") or not os.getenv("NEO4J_URI") or not os.getenv("NEO4J_USERNAME") or not os.getenv("NEO4J_PASSWORD"):
+    raise ValueError("Missing one or more environment variables.")
 
 env_config = {
-    "GROQ_API_KEY": os.getenv("GROQ_API_KEY")
+    "GROQ_API_KEY": os.getenv("GROQ_API_KEY"),
+    "NEO4J_URI": os.getenv("NEO4J_URI"),
+    "NEO4J_USERNAME": os.getenv("NEO4J_USERNAME"),
+    "NEO4J_PASSWORD": os.getenv("NEO4J_PASSWORD"),
+    "GROQ_MODEL": "openai/gpt-oss-120b",
+    "GROQ_BASE_URL": "https://api.groq.com/openai/v1",
+    "EMBEDDER_MODEL": "nomic-embed-text"
 }
