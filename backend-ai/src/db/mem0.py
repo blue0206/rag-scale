d60e1fc8f9e587e29b17cb7f9524978acb82ffa9
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-04 16:15:32 +0530

    feat: setup mem0 client with config

---
+++ backend-ai/src/db/mem0.py
@@ -0,0 +1,38 @@
+from mem0 import Memory
+from ..core.config import env_config
+
+config = {
+    "vector_store": {
+        "provider": "qdrant",
+        "config": {
+            "collection_name": "mem0_store",
+            "host": "localhost",
+            "port": 6333,
+            "embedding_model_dims": 768
+        }
+    },
+    "graph_store": {
+        "provider": "neo4j",
+        "config": {
+            "url": env_config["NEO4J_URI"],
+            "username": env_config["NEO4J_USERNAME"],
+            "password": env_config["NEO4J_PASSWORD"]
+        }
+    },
+    "llm": {
+        "provider": "groq",
+        "config": {
+            "model": env_config["GROQ_MODEL"],
+            "api_key": env_config["GROQ_API_KEY"]
+        }
+    },
+    "embedder": {
+        "provider": "ollama",
+        "config": {
+            "model": env_config["EMBEDDER_MODEL"],
+            "ollama_base_url": "http://localhost:11434"
+        }
+    }
+}
+
+mem0_client = Memory.from_config(config)
