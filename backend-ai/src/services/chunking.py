c93801379308f73e2ef18deb4bf19a50fec31d7b
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-04 21:15:21 +0530

    feat: update load_file function to store metadata in docs
    - The docs metadata now have both user_id and pdf_id so that all embeddings can be stored in a single cluster without losing track of their corresponding users.

--- backend-ai/src/services/chunking.py
+++ backend-ai/src/services/chunking.py
@@ -2,15 +2,21 @@ from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-def load_file(id: str) -> list:
+def load_file(user_id: str, pdf_id: str) -> list:
     """
     Loads the pdf file from files directory and returns the document.
     """
 
-    path = Path(__file__).parent.parent / "files" / f"{id}.pdf"
+    path = Path(__file__).parent.parent / "files" / f"{user_id}_{pdf_id}.pdf"
     loader = PyPDFLoader(path)
 
     docs = loader.load()
+    
+    # Store user_id and pdf_id in metadata for proper retrieval.
+    for doc in docs:
+        doc.metadata["user_id"] = user_id
+        doc.metadata["pdf_id"] = pdf_id
+
     return docs
 
 def split_file(docs: list) -> list:
