8f80047ae3aebfdaeef9b0e034b556e6aa431d2f
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-04 21:22:10 +0530

    feat: add function to process chunks
    - This function generates vector embeddings from chunks.
    - The vector embeddings are then stored in vector database.

---
+++ backend-ai/src/workers/ingestion_worker.py
@@ -0,0 +1,16 @@
+from langchain_ollama import OllamaEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from ..core.config import env_config
+
+def process_chunks(chunks: list) -> None:
+    embeddings = OllamaEmbeddings(
+        model=env_config["EMBEDDER_MODEL"],
+        base_url="http://localhost:11434"
+    )
+
+    QdrantVectorStore.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        url="http://localhost:6333",
+        collection_name="file_embeddings"
+    )
