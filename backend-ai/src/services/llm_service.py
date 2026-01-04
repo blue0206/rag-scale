8235614198d6ffa4b9aec07516a992daeebb2745
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-04 23:01:06 +0530

    feat: add retrieval query node to langgraph workflow
    - A vector similarity search is performed based on user query.
    - Th search results are provided as context to LLM for response generation.

--- backend-ai/src/services/llm_service.py
+++ backend-ai/src/services/llm_service.py
@@ -1,4 +1,7 @@
+from qdrant_client.models import Filter, FieldCondition, MatchValue
 from typing import Literal
+from langchain_ollama import OllamaEmbeddings
+from langchain_qdrant import QdrantVectorStore
 from ..core.config import env_config
 from ..models.chat import State
 from ..core.llm_client import llm_client
@@ -19,11 +22,13 @@ def classify_query(state: State) -> State:
 
     In case you are unable to make out the type of query, return 'NORMAL'.
     """
+
+    state["messages"] = {"role": "user", "content": state.get("user_query")}
     
     response = llm_client.responses.create(
         model=env_config["GROQ_MODEL"],
         instructions=SYSTEM_PROMPT,
-        input=[{"role": "user", "content": state["user_query"]}],
+        input=state.get("messages")
     )
 
     state["query_type"] = response.output[-1].content[-1].text.strip().upper()
@@ -39,3 +44,55 @@ def route_query(state: State) -> Literal["NORMAL", "RETRIEVAL"]:
     
     return "NORMAL"
 
+def retrieval_query(state: State) -> State:
+    """
+    Converts user query into vector embeddings and performs a vector similarity
+    search to retrieve relevant data from vector database to answer the query.
+    The retrieved data is then sent to the LLM to generate a response.
+    """
+
+    embeddings = OllamaEmbeddings(
+        model=env_config["EMBEDDER_MODEL"],
+        base_url="http://localhost:11434",
+    )
+
+    vector_db = QdrantVectorStore.from_existing_collection(
+        url="http://localhost:6333",
+        collection_name=env_config["RAG_COLLECTION_NAME"],
+        embedding=embeddings
+    )
+
+    search_results = vector_db.similarity_search(
+        query=state.get("user_query"), 
+        filter=Filter(
+            must=[
+                FieldCondition(
+                    key="user_ID",
+                    match=MatchValue(value=state.get("user_id"))
+                )
+            ]
+        )
+    )
+
+    context = [f"Page Content: {result.page_content}\nPage Label: {result.metadata.get("page_label")}" for result in search_results]
+
+    SYSTEM_PROMPT = f"""
+    You are an expert AI Assistant. You will receive a user query.
+    You have to answer the query based on the context provided.
+
+    If applicable and available, also provide the page number where the answer is found.
+
+    If context not available, try to answer the query based on your general knowledge, else return a helpful message.
+
+    Context:
+    {context}
+    """
+
+    response = llm_client.responses.create(
+        model=env_config["GROQ_MODEL"],
+        instructions=SYSTEM_PROMPT,
+        input=state.get("messages"),
+    )
+
+    state["messages"] = {"role": "assistant", "content": response.output[-1].content[-1].text}
+    return state
