ec005c15692faf2d43fc8a4717b113f88aac3100
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-05 15:44:23 +0530

    fix: remove Annotated type from messages
    - The messages will be sent as chunks via SSE and hence will now be manually appended.

--- backend-ai/src/models/chat.py
+++ backend-ai/src/models/chat.py
@@ -1,8 +1,8 @@
 from operator import add
-from typing_extensions import TypedDict, Annotated, Literal, Optional
+from typing_extensions import TypedDict, Literal, Optional
 
 class State(TypedDict):
     user_id: str
     user_query: str
-    messages: Annotated[list, add]
+    messages: list
     query_type: Optional[Literal["NORMAL", "RETRIEVAL"]]
