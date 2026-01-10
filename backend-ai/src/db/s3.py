0470364addee8b7f1b269f349f90bebb8bd58279
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-11 00:46:19 +0530

    fix: replace boto3 with aioboto3 for asyncio support

--- backend-ai/src/db/s3.py
+++ backend-ai/src/db/s3.py
@@ -1,7 +1,7 @@
-import boto3
+import aioboto3
 from ..core.config import env_config
 
-s3_client = boto3.client(
+s3_client = aioboto3.client(
     "s3",
     endpoint_url="https://minio:9000",
     aws_access_key_id=env_config["S3_ACCESS_KEY_ID"],
