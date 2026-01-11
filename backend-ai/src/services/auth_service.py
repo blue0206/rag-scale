df881ee30783258f1ebe3f449bbd2bc88e9dbfd5
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-11 14:26:08 +0530

    refactor: extract session token generation to a separate function

--- backend-ai/src/services/auth_service.py
+++ backend-ai/src/services/auth_service.py
@@ -1,6 +1,6 @@
 import secrets
 from passlib.apps import custom_app_context as pwd_context
-from uuid import uuid4
+from uuid import uuid4, UUID
 from ..db.mongo import users_collection, sessions_collection
 from models.api import RegisterRequestBody
 from models.auth import UserInDB, SessionInDB, RegisterServiceResponse
@@ -16,27 +16,32 @@ async def register_user(user_data: RegisterRequestBody) -> RegisterServiceRespon
     # hash password
     hash = pwd_context.hash(user_data.password)
 
-    # Create user
+    # Create user and generate session token for auth.
     try:
         new_user: UserInDB = await users_collection.insert_one(
             {"id": uuid4(), "username": user_data.username, "password": hash}
         )
+
+        session_token = await generate_session_token(new_user.id)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-    # Generate session token for auth.
+    return RegisterServiceResponse(
+        user_id=new_user.id, username=new_user.username, session_token=session_token
+    )
+
+
+async def generate_session_token(user_id: UUID) -> str:
     try:
         session_token = secrets.token_hex(32)
         session: SessionInDB = await sessions_collection.insert_one(
             {
                 "token": session_token,
-                "user_id": new_user.id,
+                "user_id": user_id,
                 "expires_at": datetime.now() + timedelta(days=1),
             }
         )
+
+        return session.token
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-
-    return RegisterServiceResponse(
-        user_id=new_user.id, username=new_user.username, session_token=session.token
-    )
