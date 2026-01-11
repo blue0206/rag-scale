104057ed998e3402fb880431038426853c421df4
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-11 15:11:17 +0530

    feat: add function for user login in auth service
    - The function accepts the request body as it is.
    - The user is fetched from DB using the username.
    - If the user does not exist or password doesn't match hashed pass, then we raise an error.
    - Else, the function returns the id of user, the username, and the session token.

--- backend-ai/src/services/auth_service.py
+++ backend-ai/src/services/auth_service.py
@@ -1,9 +1,11 @@
 import secrets
+from pymongo.errors import DuplicateKeyError
 from passlib.apps import custom_app_context as pwd_context
 from uuid import uuid4, UUID
-from ..db.mongo import users_collection, sessions_collection
 from models.api import AuthRequestBody
 from models.auth import UserInDB, SessionInDB, AuthServiceResponse
+from fastapi import HTTPException
+from ..db.mongo import users_collection, sessions_collection
 
 
 async def register_user(user_data: AuthRequestBody) -> AuthServiceResponse:
@@ -23,11 +25,42 @@ async def register_user(user_data: AuthRequestBody) -> AuthServiceResponse:
         )
 
         session_token = await generate_session_token(new_user.id)
+    except DuplicateKeyError:
+        raise HTTPException(status_code=400, detail="Username already exists.")
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=500, detail="Something went wrong.")
 
-    return RegisterServiceResponse(
+    return AuthServiceResponse(
         user_id=new_user.id, username=new_user.username, session_token=session_token
+    )
+
+
+async def login_user(user_data: AuthRequestBody) -> AuthServiceResponse:
+    """
+    This function logs in a user by first fetching the user details from
+    database, then comparing the password with the hashed password.
+    If the password is correct, it generates a session token and returns the
+    user id, username, and session token, else it raises an error.
+    """
+
+    # Get user details from database.
+    try:
+        user: UserInDB = await users_collection.find_one({"username": user_data.username})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Something went wrong.")
+
+    # Check if user exists and password is correct.
+    if not user or not pwd_context.verify(user_data.password, user.password):
+        raise HTTPException(status_code=400, detail="Invalid username or password.")
+
+    # Generate session token and return response.
+    try:
+        session_token = await generate_session_token(user.id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Something went wrong.")
+
+    return AuthServiceResponse(
+        user_id=user.id, username=user.username, session_token=session_token
     )
 
 
@@ -37,17 +70,13 @@ async def generate_session_token(user_id: UUID) -> str:
     session token and corresponding user id with expiry time.
     """
 
-    try:
-        session_token = secrets.token_hex(32)
-        session: SessionInDB = await sessions_collection.insert_one(
-            {
-                "token": session_token,
-                "user_id": user_id,
-                "expires_at": datetime.now() + timedelta(days=1),
-            }
-        )
+    session_token = secrets.token_hex(32)
+    session: SessionInDB = await sessions_collection.insert_one(
+        {
+            "token": session_token,
+            "user_id": user_id,
+            "expires_at": datetime.now() + timedelta(days=1),
+        }
+    )
 
-        return session.token
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
+    return session.token
