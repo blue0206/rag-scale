2982723d202aa5be6d349283120f681cbebdc169
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-11 14:54:10 +0530

    feat: setup lifespan event to run db indexing on startup

--- backend-ai/main.py
+++ backend-ai/main.py
@@ -2,8 +2,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import uvicorn
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from core.db import setup_db_index
 from src.models.api import ApiResponse
 
 app = FastAPI()
@@ -17,8 +19,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await setup_db_index()
+    yield
+
 @app.get("/")
 def root_endpoint():
     return ApiResponse(success=True, status_code=200, message="RagScale server is running.")
 
-uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, lifespan=lifespan)
