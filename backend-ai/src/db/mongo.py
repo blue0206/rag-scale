3a25c32ab7286303d2eb5c91ffe11b40e0b5e968
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-11 12:29:29 +0530

    feat: setup mongo client

---
+++ backend-ai/src/db/mongo.py
@@ -0,0 +1,7 @@
+import motor.motor_asyncio
+from ..core.config import env_config
+
+client = motor.motor_asyncio.AsyncIOMotorClient(f"mongodb://{env_config["MONGO_DB_ROOT_USERNAME"]}:{env_config["MONGO_DB_ROOT_PASSWORD"]}@mongo:27017/ragscale_db?authSource=admin")
+mongo_client = client.ragscale_db
+
+users_collection = mongo_client.get_collection("users")
