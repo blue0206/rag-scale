242f913414ab457c897edf961eaeb7cd9bca55c1
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-11 14:19:22 +0530

    feat: add response type for register function in auth service

--- backend-ai/src/models/auth.py
+++ backend-ai/src/models/auth.py
@@ -2,12 +2,20 @@ from datetime import datetime
 from pydantic import BaseModel
 from uuid import UUID
 
+
 class UserInDB(BaseModel):
     id: UUID
     username: str
     password: str
 
+
 class SessionInDB(BaseModel):
     token: str
     user_id: UUID
     expires_at: datetime
+
+
+class RegisterServiceResponse(BaseModel):
+    user_id: UUID
+    username: str
+    session_token: str
