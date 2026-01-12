8058dbd34dd4051d667b50631dbbbd6f5212a8f7
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-12 15:37:50 +0530

    fix: reorder imports and update root endpoint response model

--- backend-ai/main.py
+++ backend-ai/main.py
@@ -1,13 +1,12 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 import uvicorn
+from dotenv import load_dotenv
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from core.db import setup_db_index
+from src.core.db import setup_db_index
 from src.models.api import ApiResponse
 
+load_dotenv()
 app = FastAPI()
 
 # cors
@@ -20,12 +19,12 @@ app.add_middleware(
 )
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(_: FastAPI):
     await setup_db_index()
     yield
 
-@app.get("/")
-def root_endpoint():
-    return ApiResponse(success=True, status_code=200, message="RagScale server is running.")
+@app.get("/", response_model=ApiResponse[str])
+def root_endpoint() -> ApiResponse[str]:
+    return ApiResponse(success=True, status_code=200, payload="RagScale server is running.")
 
-uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, lifespan=lifespan)
+uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, lifespan='on')
