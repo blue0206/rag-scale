84a4f338cf86f9b0622687899d772a05edf8fae6
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-15 01:00:32 +0530

    feat: update load_file function to download PDF from S3 for loading

--- backend-ai/src/workers/chunking_worker.py
+++ backend-ai/src/workers/chunking_worker.py
@@ -1,25 +1,53 @@
-from pathlib import Path
+import os
+from typing import List
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from ..db.redis import embedding_queue
 from .embedding_worker import process_chunks
+from ..db.s3 import s3_client
 
-def load_file(user_id: str, pdf_id: str) -> list:
+FILES_DIR = "/tmp/ragscale_downloads"
+os.makedirs(FILES_DIR, exist_ok=True)
+
+def load_file(user_id: str, batch_id: str, object_key: str, bucket_name: str) -> List:
     """
-    Loads the pdf file from files directory and returns the document.
+    This function downloads the PDF file from S3 and temporarily saves to disk.
+    The saved file is loaded using PyPDFLoader and then deleted from disk.
+    The function returns the loaded document with user_id and batch_id stored in metadata.
+
+    This function accepts the following parameters:
+    - user_id: ID of the user.
+    - batch_id: ID of the batch.
+    - object_key: S3 object key where the PDF is stored.
+    - bucket_name: Name of the S3 bucket.
     """
 
-    path = Path(__file__).parent.parent.parent / "files" / f"{user_id}_{pdf_id}.pdf"
-    loader = PyPDFLoader(path)
+    # Load pdf from S3 bucket.
+    path = os.path.join(FILES_DIR, object_key.replace("/", "_"))
 
-    docs = loader.load()
+    print(f"Downloading file from s3://{bucket_name}/{object_key} to {path}.")
     
-    # Store user_id and pdf_id in metadata for proper retrieval.
+    s3_client.download_file(
+        Bucket=bucket_name,
+        Key=object_key,
+        Filename=path
+    )
+
+    print("File downloaded. Loading document.")
+
+    loader = PyPDFLoader(path)
+    docs = loader.load()
+
+    os.remove(path)
+    print("Document loaded successfully. Temporary file removed.")
+    
+    # Store user_id and batch_id in metadata for proper retrieval.
     for doc in docs:
         doc.metadata["user_id"] = user_id
-        doc.metadata["pdf_id"] = pdf_id
+        doc.metadata["batch_id"] = batch_id
 
     return docs
+
 
 def split_file(docs: list) -> list:
     """
