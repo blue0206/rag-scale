0537b69dcb5b8e06631c7814603734b71de94a41
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-15 01:06:55 +0530

    refactor: update load_file and split_file function signatures for type hinting

--- backend-ai/src/workers/chunking_worker.py
+++ backend-ai/src/workers/chunking_worker.py
@@ -1,15 +1,14 @@
 import os
 from typing import List
+from langchain_core.documents import Document
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from ..db.redis import embedding_queue
-from .embedding_worker import process_chunks
 from ..db.s3 import s3_client
 
 FILES_DIR = "/tmp/ragscale_downloads"
 os.makedirs(FILES_DIR, exist_ok=True)
 
-def load_file(user_id: str, batch_id: str, object_key: str, bucket_name: str) -> List:
+def load_file(user_id: str, batch_id: str, object_key: str, bucket_name: str) -> List[Document]:
     """
     This function downloads the PDF file from S3 and temporarily saves to disk.
     The saved file is loaded using PyPDFLoader and then deleted from disk.
@@ -49,29 +48,17 @@ def load_file(user_id: str, batch_id: str, object_key: str, bucket_name: str) ->
     return docs
 
 
-def split_file(docs: list) -> list:
+def split_file(docs: List[Document]) -> List[Document]:
     """
-    Splits the document into chunks.
+    This function accepts a list of Documents and splits the documents into chunks.
     """
+
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=400
     )
 
+    print("Splitting document into chunks.")
+
     chunks = text_splitter.split_documents(docs)
     return chunks
-
-def offload_chunks(chunks: list) -> list:
-    """
-    Sends the chunks to the chunking queue.
-    """
-    
-    n = len(chunks)
-    jobs = []
-
-    for i in range(0, n, 20):
-        chunk_subset = chunks[i:i+20]
-        job = embedding_queue.enqueue(process_chunks, chunk_subset)
-        jobs.append(job)
-
-    return jobs
