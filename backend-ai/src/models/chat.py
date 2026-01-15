825401c18c64684eb81f30e19b3acff402332a51
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-16 01:10:20 +0530

    feat: Annotate langgraph state messages

--- backend-ai/src/models/chat.py
+++ backend-ai/src/models/chat.py
@@ -1,7 +1,9 @@
-from typing_extensions import TypedDict, Literal, Optional, List
+from operator import add
+from openai.types.responses import ResponseInputParam
+from typing_extensions import TypedDict, Literal, Optional, Annotated
 
 class State(TypedDict):
     user_id: str
     user_query: str
-    messages: List
+    messages: Annotated[ResponseInputParam, add]
     query_type: Optional[Literal["NORMAL", "RETRIEVAL"]]
