7cad9aad1901fec677c557916cad6b4502cea938
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2026-01-16 15:41:17 +0530

    feat: implement async STT with S3 pre-signed URL
    Refactors the speech-to-text service to be fully asynchronous using AsyncGroq.
    
    - Replaces local file handling with a robust, cloud-native workflow:
      - Uploads audio files to MinIO
      - Generates a pre-signed URL to grant temporary access.
      - Passes the URL to the STT API, avoiding local disk writes.
    - Adds logic to delete the audio file from S3 after transcription to save space.

--- backend-ai/src/services/voice_agent.py
+++ backend-ai/src/services/voice_agent.py
@@ -1,25 +1,47 @@
 import os
+from fastapi import UploadFile
+from uuid import uuid4
+from groq import AsyncGroq
+from ..db.s3 import s3_client
 from ..core.config import env_config
-from groq import Groq
-
-groq_client = Groq(api_key=env_config["GROQ_API_KEY"])
 
 
-def speech_to_text(filename: str):
+groq_client = AsyncGroq(api_key=env_config["GROQ_API_KEY"])
+
+
+async def speech_to_text(file: UploadFile):
     """
-    This function receives a filename for an audio file of a user query and
-    returns the translated text.
+    This function receives an audio file of a user query. The file
+    is uploaded to S3, its presigned url is generated and sent to STT api.
     """
 
-    with open(filename, "rb") as f:
-        translation = groq_client.audio.translations.create(
-            file=f, model="whisper-large-v3"
-        )
+    file_content = await file.read()
+    BUCKET = "ragscale-audio"
+    KEY = f"input_audio/{str(uuid4())}_{file.filename}"
+
+    await s3_client.upload_file_async(
+        bucket=BUCKET,
+        key=KEY,
+        file=file_content
+    )
+
+    presigned_url = await s3_client.create_presigned_url(
+        bucket=BUCKET,
+        key=KEY,
+    )
+
+    translation = await groq_client.audio.translations.create(
+        url=presigned_url, 
+        model="whisper-large-v3"
+    )
 
     try:
-        os.remove(filename)
+        await s3_client.delete_file_async(
+            bucket=BUCKET,
+            key=KEY
+        )
     except Exception as e:
-        print(f"Error deleting file {filename}: {e}")
+        print(f"Error deleting file {file.filename}: {e}")
 
     return translation.text
 
